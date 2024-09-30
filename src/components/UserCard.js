57493d9c9ad582ed239b2c766bc112ec34599b05
viniponteslima <1999vini1999@gmail.com>
2024-09-29 21:33:58 -0300

    Adicionado UserCard.

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,18 @@
 import styles from './UserCard.module.css';
+import image from '../images/user.png'
 
 export default function UserCard() {
+  const user = {
+    name: 'Vinícius Pontes Lima',
+    carrer: 'Front-End Developer',
+  }
+
   return (
-    <header className={styles.header}>
-      
+    <header className={styles.userCard}>
+      <img src={image} alt='User' />
+      {user.name && ( <h1>{user.name}</h1> )}
+      {user.carrer && ( <h2>{user.carrer}</h2> )}
+      {user.about && ( <p>{user.about}</p> )}
     </header>
   );
 }
